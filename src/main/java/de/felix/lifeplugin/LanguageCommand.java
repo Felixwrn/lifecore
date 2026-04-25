29681dc02b4816042452f4cbdb1b2c9658c1ab98
Felixwrn <104013013+Felixwrn@users.noreply.github.com>
2026-04-25 20:36:40 +0200

    Implement LanguageCommand for language selection

---
+++ src/main/java/de/felix/lifeplugin/LanguageCommand.java
@@ -0,0 +1,38 @@
+package de.felix.lifecore.commands;
+
+import de.felix.lifecore.lang.LanguageManager;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class LanguageCommand {
+
+    private final LanguageManager lang;
+
+    public LanguageCommand(LanguageManager lang) {
+        this.lang = lang;
+    }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (!(sender instanceof Player p)) return true;
+
+        if (args.length == 0) {
+            p.sendMessage("§cUsage: /language <de|en>");
+            return true;
+        }
+
+        String langCode = args[0].toLowerCase();
+
+        if (!langCode.equals("de") && !langCode.equals("en")) {
+            p.sendMessage("§cUnknown language!");
+            return true;
+        }
+
+        lang.setLanguage(p.getUniqueId(), langCode);
+
+        p.sendMessage("§aLanguage set to: " + langCode);
+
+        return true;
+    }
+}
