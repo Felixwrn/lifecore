b5de9e8753c95f21aaf2654e60ccd41192f337ab
Felixwrn <104013013+Felixwrn@users.noreply.github.com>
2026-04-29 14:13:57 +0200

    Update ActionBarUtil.java

--- src/main/java/de/felix/lifeplugin/util/ActionBarUtil.java
+++ src/main/java/de/felix/lifeplugin/util/ActionBarUtil.java
@@ -1,17 +1,15 @@
 package de.felix.lifeplugin.util;
 
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
 import org.bukkit.entity.Player;
 
 public class ActionBarUtil {
 
     public static void send(Player p, String msg) {
-        try {
-            p.sendActionBar(msg);
-        } catch (NoSuchMethodError e) {
-            p.spigot().sendMessage(
-                net.md_5.bungee.api.ChatMessageType.ACTION_BAR,
-                new net.md_5.bungee.api.chat.TextComponent(msg)
-            );
-        }
+        p.spigot().sendMessage(
+                ChatMessageType.ACTION_BAR,
+                new TextComponent(msg)
+        );
     }
 }
