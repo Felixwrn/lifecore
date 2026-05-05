29c7b34bdb032be9f095715c8692c909e98dfc03
Felixwrn <104013013+Felixwrn@users.noreply.github.com>
2026-05-05 19:31:49 +0200

    Add command handling for multiple GUIs

--- src/main/java/de/felix/lifeplugin/Main.java
+++ src/main/java/de/felix/lifeplugin/Main.java
@@ -1,6 +1,14 @@
 package de.felix.lifeplugin;
 
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import de.felix.lifeplugin.gui.LanguageGUI;
+import de.felix.lifeplugin.gui.LifeGUI;
+import de.felix.lifeplugin.gui.ModeGUI;
+import de.felix.lifeplugin.gui.MarketplaceGUI;
 
 public class Main extends JavaPlugin {
 
@@ -14,5 +22,33 @@ public class Main extends JavaPlugin {
     public void onEnable() {
         instance = this;
         getLogger().info("LifePlugin gestartet!");
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+        if (!(sender instanceof Player p)) return true;
+
+        if (cmd.getName().equalsIgnoreCase("livesgui")) {
+            LifeGUI.open(p);
+            return true;
+        }
+
+        if (cmd.getName().equalsIgnoreCase("langgui")) {
+            LanguageGUI.open(p);
+            return true;
+        }
+
+        if (cmd.getName().equalsIgnoreCase("modegui")) {
+            ModeGUI.open(p);
+            return true;
+        }
+
+        if (cmd.getName().equalsIgnoreCase("market")) {
+            MarketplaceGUI.open(p);
+            return true;
+        }
+
+        return false;
     }
 }
