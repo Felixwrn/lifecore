5f2222ca932188316db7283b98b0c30fa3e6d939
Felixwrn <104013013+Felixwrn@users.noreply.github.com>
2026-05-07 22:12:16 +0200

    challengelistner.java aktualisieren

--- src/main/java/de/felix/lifeplugin/challenge/challengelistner.java
+++ src/main/java/de/felix/lifeplugin/challenge/challengelistner.java
@@ -1,1 +1,53 @@
+package de.felix.lifeplugin.challenge;
 
+import de.felix.lifeplugin.Main;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+public class ChallengeListener implements Listener {
+
+    @EventHandler
+    public void onBreak(BlockBreakEvent e) {
+
+        Player p = e.getPlayer();
+
+        if (e.getBlock().getType() != Material.STONE) {
+            return;
+        }
+
+        int progress = Main.getInstance().getConfig().getInt(
+                "challenge-progress." + p.getUniqueId() + ".stone_miner",
+                0
+        );
+
+        progress++;
+
+        Main.getInstance().getConfig().set(
+                "challenge-progress." + p.getUniqueId() + ".stone_miner",
+                progress
+        );
+
+        Main.getInstance().saveConfig();
+
+        Challenge challenge = ChallengeManager
+                .getChallenges()
+                .get("stone_miner");
+
+        if (challenge == null) {
+            return;
+        }
+
+        if (progress >= challenge.getGoal()) {
+
+            p.sendMessage("§aChallenge completed!");
+
+            p.sendMessage(
+                    "§eReward: §6" +
+                            challenge.getReward()
+            );
+        }
+    }
+}
